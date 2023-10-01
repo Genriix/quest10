43ef2d71ddc74e7b90e55ae4130e1b769db3c818
Vladislav Il'in <vladi5lav.ilin@yandex.ru>
2023-10-01 18:52:57 +0300

    v3.0

--- questTest/Program.cs
+++ questTest/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace quest10._3
+namespace questTest
 {
     class Title
     {
