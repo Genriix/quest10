1ab5258234b1be149bf5e1ec0f36857ab91e91b8
Vladislav Il'in <vladi5lav.ilin@yandex.ru>
2023-10-01 16:55:28 +0300

    v2.0.1

--- quest10.1/Properties/AssemblyInfo.cs
+++ quest10.1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("quest10")]
+[assembly: AssemblyTitle("quest10.1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("quest10")]
+[assembly: AssemblyProduct("quest10.1")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
-[assembly: Guid("9682f161-e5a2-4976-a43f-2b0da8846b55")]
+[assembly: Guid("9585b3ef-b578-4e1b-a1c1-91e3981f4c5b")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
