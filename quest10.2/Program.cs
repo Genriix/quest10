08afd0f630b5801a7b8da7b82fac4a08253637e9
Vladislav Il'in <vladi5lav.ilin@yandex.ru>
2023-10-01 15:35:26 +0300

    v1.2

---
+++ quest10.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quest10._2
+{
+    class Rectangle
+    {
+        double side1;
+        double side2;
+        double area;
+        double perimeter;
+
+        public Rectangle(double side1, double side2)
+        {
+            Console.WriteLine("Пользовательский конструктор");
+            this.side1 = side1;
+            this.side2 = side2;
+            AreaCalculator(side1, side2);
+            PerimeterCalculator(side1, side2);
+        }
+
+        public double Area { get { return area; } }
+        public double Perimeter { get { return perimeter; } }
+
+        public double AreaCalculator(double side1, double side2) { return area = side1 * side2; }
+
+        public double PerimeterCalculator(double side1, double side2) {  return perimeter = 2 * (side1 + side2); }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины двух сторон прямоугольника С ЗАПЯТОЙ!!!!!");
+            Rectangle rectagle = new Rectangle(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+            Console.WriteLine($"S прямоугольника: {rectagle.Area}");
+            Console.WriteLine($"P прямоугольника: {rectagle.Perimeter}");
+
+            Console.ReadKey();
+        }
+    }
+}
--- quest10/Program.cs
+++ quest10/Program.cs
@@ -5,11 +5,69 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace quest10
-{
+{        
+    class Address
+        {
+            private int index;
+
+            public int Index
+            {
+                set { index = value; }
+                get { return index; }
+            }
+            private string country = null;
+
+            public string Country
+            {
+                get { return country; }
+                set { country = value; }
+            }
+
+            private string city = null;
+            public string City
+            {
+                get { return city; }
+                set { city = value; }
+            }
+            private string street = null;
+            public string Street
+            {
+                get { return street; }
+                set { street = value; }
+            }
+            private int house;
+            public int House
+            {
+                get {  return house; }
+                set { house = value; }
+            }
+
+            private int apartment;
+
+            public int Apartment
+            {
+                get { return apartment; }
+                set { apartment = value; }
+            }
+        }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Address address = new Address();
+            address.Index = 123;
+            address.Country = "Россия";
+            address.City = "Воркута";
+            address.Street = "Лермонтова";
+            address.House = 23;
+            address.Apartment = 10;
+            Console.WriteLine($"Почтовый индекс: " + address.Index);
+            Console.WriteLine($"Страна: " + address.Country);
+            Console.WriteLine($"Город: " + address.City);
+            Console.WriteLine($"Улица: " + address.Street);
+            Console.WriteLine($"Дом: " + address.House);
+            Console.WriteLine($"Квартира: " + address.Apartment);
         }
     }
 }
